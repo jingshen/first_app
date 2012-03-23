26ba9b915043c689d6b56dc540f832faf645d33c
jingshen <jingshen.ng@gmail.com>
2012-03-23 11:18:29 +0800

    finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 FirstApp::Application.routes.draw do
-  get "users/new"
-
+  resources :users
   root to: 'static_pages#home'
 
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
   match '/signup', to: 'users#new'
-
-  resources :microposts
-
-  resources :users
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
