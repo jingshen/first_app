555849dcb43359c33325a17e5e2e2f7096edbc19
jingshen <jingshen.ng@gmail.com>
2012-03-23 21:50:50 +0800

    changed from cookies to session

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,6 @@
 module SessionsHelper
   def sign_in(user)
-    cookies.permanent[:remember_token] = user.remember_token
+    session[:remember_token] = user.remember_token
     current_user = user
   end
   def current_user=(user)
@@ -17,13 +17,13 @@ module SessionsHelper
 
   def sign_out
     current_user = nil
-    cookies.delete(:remember_token)
+    session[:remember_token] = nil
   end
 
   private
 
     def user_from_remember_token
-      remember_token = cookies[:remember_token]
+      remember_token = session[:remember_token]
       User.find_by_remember_token(remember_token) unless remember_token.nil?
     end
 end
